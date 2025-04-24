c02417eea145ac7455cb4d1c6c34fde8377da966
lmromero18 <lmromero.18@est.ucab.edu.ve>
2025-04-24 15:35:31 -0400

    Ajuste cambio de nombre carpeta

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createProducts, deleteProduct, getPaginatedProducts, getProductById, getProducts, updateProduct } from '../controllers/product.controller';
-import asyncHandler from '../helpers/async.handler'; 
+import asyncHandler from '../handlers/async.handler'; 
 
 const router = express.Router();
 
